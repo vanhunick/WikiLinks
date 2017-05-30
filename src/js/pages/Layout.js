504a93099080a67d7a718703a7b6f51bf51e3491
Nicky van Hulst <nickyvanhulst@gmail.com>
2017-05-30 18:53:27 +1200

    Added navbar component

--- src/js/pages/Layout.js
+++ src/js/pages/Layout.js
@@ -4,6 +4,8 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 import PageTitle from "../components/PageTitle"
 import BarChart from "../components/BarTest";
+import NavBar from "../components/Header/NavBar";
+
 
 export default class Layout extends React.Component {
   constructor() {
@@ -23,7 +25,9 @@ export default class Layout extends React.Component {
 
   render() {
     return (
+      
       <div className="container myContainer text-center">
+        <NavBar />  
         <PageTitle title={"Search a wiki article to generate connection graph"}/>
       <BarChart data={[5,10,1,3]} size={[500,500]} />
       <Footer />
